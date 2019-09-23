53c960b8d6593a054bf7b11db2c432ce9ec55d69
DevFreddy <michael.frederickjr@gmail.com>
2019-09-23 13:45:46 -0400

    Initializing nerdpack via nr1 cli

---
+++ meande/nerdlets/meande-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class Meande extends React.Component {
+    render() {
+        return <h1>Hello, meande Nerdlet!</h1>;
+    }
+}

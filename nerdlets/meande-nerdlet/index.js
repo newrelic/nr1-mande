41d61a4696f0e22a61e1696db74ee148ba2e0e35
DevFreddy <michael.frederickjr@gmail.com>
2019-09-26 16:22:26 -0400

    Adding AccountPicker, some layout, and initial charts

--- nerdlets/meande-nerdlet/index.js
+++ nerdlets/meande-nerdlet/index.js
@@ -1,35 +1,96 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import EventStream from '../../components/EventStream';
+
+import {
+  Grid,
+  GridItem,
+  NerdGraphQuery,
+  Stack,
+  StackItem,
+  LineChart,
+} from 'nr1';
+
+import AccountPicker from '../../components/account-picker';
 import Board from '../../components/board';
-
-import { Button, Icon } from 'nr1';
-
-// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class Meande extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      accounts: [],
+      account: { name: 'Choose an account' },
+      accountId: null,
+    };
+
+    this.setAccount = this.setAccount.bind(this);
   }
 
-  //   openMediaSessionExplorer() {
-  //     const { entity } = this.props;
-  //     navigation.openStackedNerdlet({
-  //       id: 'media-session-explorer',
-  //       urlState: {
-  //         pageUrl,
-  //         entityGuid: entity.guid,
-  //       },
-  //     });
-  //   }
+  async componentDidMount() {
+    const query = `
+      {
+        actor {
+          accounts {
+            name
+            id
+          }
+        }
+      }
+    `;
+    const { data } = await NerdGraphQuery.query({ query: query });
+
+    const { accounts } = data.actor;
+
+    const account = accounts.length > 0 && accounts[0];
+    this.setState({ accounts, account, accountId: account.id });
+  }
+
+  setAccount(account) {
+    const accountId = account.id;
+    this.setState({ accountId });
+  }
 
   render() {
+    const { accounts, account, accountId } = this.state;
+
     return (
-      <div>
-        <Board />
-        <EventStream />
-      </div>
+      <React.Fragment>
+        <AccountPicker
+          accounts={accounts}
+          account={account}
+          setAccount={this.setAccount}
+        ></AccountPicker>
+        {accountId && (
+          <Stack
+            fullWidth={true}
+            directionType={Stack.DIRECTION_TYPE.VERTICAL}
+            horizontalType={Stack.HORIZONTAL_TYPE.CENTER}
+            gapType={Stack.GAP_TYPE.SMALL}
+            style={{ width: '1200px' }}
+          >
+            <StackItem>
+              <Board accountId={accountId} />
+            </StackItem>
+            <StackItem style={{ width: '100%' }}>
+              <Grid>
+                <GridItem columnSpan={6}>
+                  <LineChart
+                    accountId={accountId}
+                    query="SELECT filter(count(*), WHERE actionName = 'CONTENT_ERROR') AS 'Errors', uniquecount(viewId) AS 'Concurrent Streams' FROM PageAction since 1 week ago TIMESERIES"
+                    fullWidth
+                  />
+                </GridItem>
+                <GridItem columnSpan={6}>
+                  <LineChart
+                    accountId={accountId}
+                    query="SELECT filter(count(*), WHERE actionName = 'CONTENT_ERROR') AS 'Errors', uniquecount(viewId) AS 'Concurrent Streams' FROM PageAction since 1 week ago TIMESERIES"
+                    fullWidth
+                  />
+                </GridItem>
+              </Grid>
+            </StackItem>
+          </Stack>
+        )}
+      </React.Fragment>
     );
   }
 }

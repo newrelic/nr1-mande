3298a95d14297a527e76182dc2cce5ad43d21444
Anna Strom-Olsen <aso1124@gmail.com>
2020-02-28 13:44:35 -0500

    feat: show metric detail pane
    Rename MandeContainer to MetricStackContainer
    Move state management for selected metric/stack to index.js

---
+++ nerdlets/mande-nerdlet/MetricStackContainer.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Stack, navigation } from 'nr1'
+import MetricStack from '../../components/metric/MetricStack'
+
+const metricStackContainer = props => {
+  const {
+    accountId,
+    threshold,
+    duration,
+    metricConfigs,
+    selectedStack,
+    selectMetric,
+  } = props
+
+  // convert metricConfigs into components
+  const metricStacks = metricConfigs
+    .map(config => {
+      return [...Array(config)].map((_, idx) => {
+        return (
+          <MetricStack
+            key={config.title + idx}
+            config={config}
+            accountId={accountId}
+            threshold={threshold}
+            duration={duration}
+            selected={selectedStack && selectedStack.title === config.title}
+            selectMetric={selectMetric}
+          />
+        )
+      })
+    })
+    .reduce((arr, val) => {
+      return arr.concat(val)
+    }, [])
+
+  return (
+    <Stack
+      fullWidth={true}
+      directionType={Stack.DIRECTION_TYPE.HORIZONTAL}
+      horizontalType={Stack.HORIZONTAL_TYPE.FILL_EVENLY}
+    >
+      {metricStacks}
+    </Stack>
+  )
+}
+
+export default metricStackContainer
--- nerdlets/mande-nerdlet/index.js
+++ nerdlets/mande-nerdlet/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 import { PlatformStateContext, NerdGraphQuery, Stack, StackItem } from 'nr1'
 
-import MandeContainer from './MandeContainer'
 import DimensionContainer from './DimensionContainer'
+import MetricStackContainer from './MetricStackContainer'
+import MetricDetailContainer from './MetricDetailContainer'
 
 export default class Mande extends React.Component {
   static contextType = PlatformStateContext
@@ -11,8 +12,11 @@ export default class Mande extends React.Component {
   state = {
     accountId: null,
     threshold: 'All',
+    selectedMetric: null,
+    selectedStack: null,
   }
 
+  /** INITIALIZE DATA ***************** */
   dimensionConfigs = [
     {
       name: 'Accounts',
@@ -49,14 +53,180 @@ export default class Mande extends React.Component {
     },
   ]
 
+  metricConfigs = [
+    {
+      title: 'Users',
+      navigateTo: '',
+      metrics: [
+        {
+          title: '# of Viewers',
+          query: '',
+        },
+        {
+          title: 'Stream Joins',
+          threshold: {
+            critical: 40,
+            warning: 43,
+            type: 'below',
+          },
+          invertCompareTo: 'true',
+          query: {
+            nrql: `SELECT count(*)  as 'result' FROM PageAction, MobileVideo, RokuVideo  WHERE actionName IN ('CONTENT_START', 'CONTENT_NEXT')`,
+            lookup: 'result',
+          },
+        },
+        {
+          title: 'Total View Time',
+          query: '',
+        },
+      ],
+    },
+    {
+      title: 'Platform/Client/App',
+      navigateTo: '',
+      metrics: [
+        {
+          title: 'Crash Rate',
+          query: '',
+        },
+        {
+          title: 'Javascript Error Rate',
+          query: {
+            nrql: `SELECT filter(count(*), WHERE eventType() = 'JavaScriptError') / filter(count(*), WHERE eventType() = 'PageView') * 100  as 'result' FROM JavaScriptError,PageView`,
+            lookup: 'result',
+          },
+        },
+        {
+          title: 'App Launch Time',
+          query: '',
+        },
+      ],
+    },
+    {
+      title: 'APIs',
+      navigateTo: '',
+      metrics: [
+        {
+          title: '5xx Error Rate',
+          query: {
+            nrql: `SELECT percentage(count(*), where httpResponseCode like '5%') as 'result' FROM Transaction`,
+            lookup: 'result',
+          },
+        },
+        {
+          title: 'Non 5xx Error Rate',
+          threshold: {
+            critical: 5.5,
+            warning: 4.9,
+          },
+          query: {
+            nrql: `SELECT percentage(count(*), where httpResponseCode like '4%') as 'result' FROM Transaction`,
+            lookup: 'result',
+          },
+        },
+        {
+          title: 'Latency 90th Percentile',
+          query: {
+            nrql: `SELECT percentile(duration, 90) as 'percentile' FROM Transaction`,
+            lookup: 'percentile',
+          },
+        },
+      ],
+    },
+    {
+      title: 'Video',
+      navigateTo: '',
+      metrics: [
+        {
+          title: 'Player Ready',
+          threshold: {
+            critical: 10,
+            warning: 5,
+          },
+          query: {
+            nrql: `SELECT percentile(timeSinceLoad, 50) as 'percentile' FROM PageAction`,
+            lookup: 'percentile',
+          },
+        },
+        {
+          title: 'Video Start Failure',
+          query: {
+            nrql: `SELECT count(*) AS 'result' FROM PageAction, MobileVideo, RokuVideo  WHERE actionName = 'CONTEN_ERROR' and contentPlayhead = 0`,
+            lookup: 'result',
+          },
+        },
+        {
+          title: 'Exit Before Video Start',
+          query: {
+            nrql: `SELECT filter(count(*), WHERE actionName IN ('CONTENT_REQUEST', 'CONTENT_NEXT')) - filter(count(*), WHERE actionName = 'CONTENT_START') as 'result' FROM PageAction, MobileVideo, RokuVideo`,
+            lookup: 'result',
+          },
+        },
+        {
+          title: 'Time to First Frame',
+          threshold: {
+            critical: 5,
+            warning: 3,
+          },
+          query: {
+            nrql: `SELECT percentile(timeSinceRequested/1000, 50) as 'percentile' FROM PageAction, MobileVideo, RokuVideo WHERE actionName = 'CONTENT_START'`,
+            lookup: 'percentile',
+          },
+        },
+        {
+          title: 'Rebuffer Ratio',
+          query: {
+            nrql: `SELECT filter(sum(timeSinceBufferBegin), WHERE actionName = 'CONTENT_BUFFER_END' and isInitialBuffering = 0) / filter(sum(playtimeSinceLastEvent), WHERE contentPlayhead is not null) as 'result' FROM PageAction, MobileVideo, RokuVideo`,
+            lookup: 'result',
+          },
+        },
+        {
+          title: 'Interruption Ratio',
+          query: {
+            nrql: `SELECT filter(count(*), where actionName = 'CONTENT_BUFFER_START') / filter(count(*), where actionName = 'CONTENT_START') AS 'result' FROM PageAction, MobileVideo, RokuVideo`,
+            lookup: 'result',
+          },
+        },
+      ],
+    },
+    {
+      title: 'CDN',
+      navigateTo: '',
+    },
+    {
+      title: 'Origin/Packaging',
+      navigateTo: '',
+    },
+    {
+      title: 'Ingest/Encode',
+      navigateTo: '',
+    },
+    {
+      title: 'Content/Source',
+      navigateTo: '',
+    },
+  ]
+
   query = async graphql => {
     return await NerdGraphQuery.query({ query: graphql })
+  }
+
+  onSelectMetric = selected => {
+    const stack = this.metricConfigs.filter(config => {
+      const metricFound =
+        config.metrics &&
+        config.metrics.filter(metric => metric.title === selected)
+
+      if (metricFound && metricFound.length > 0) return config
+    })
+
+    this.setState({ selectedMetric: selected, selectedStack: stack[0] })
   }
 
   render() {
     console.info('mande-nerdlet.index.render')
 
-    const { accountId, threshold } = this.state
+    const { accountId, threshold, selectedStack } = this.state
     const {
       timeRange: { duration },
     } = this.context
@@ -64,7 +234,7 @@ export default class Mande extends React.Component {
 
     return (
       <React.Fragment>
-        <DimensionContainer configs={this.dimensionConfigs}/>
+        <DimensionContainer configs={this.dimensionConfigs} />
         {accountId && (
           <Stack
             fullWidth={true}
@@ -74,12 +244,20 @@ export default class Mande extends React.Component {
             className="main-panel"
           >
             <StackItem grow>
-              <MandeContainer
+              <MetricStackContainer
                 accountId={accountId}
                 threshold={threshold}
                 duration={durationInMinutes}
+                metricConfigs={this.metricConfigs}
+                selectedStack={selectedStack}
+                selectMetric={this.onSelectMetric}
               />
             </StackItem>
+            {selectedStack && (
+              <StackItem grow>
+                <MetricDetailContainer stack={selectedStack} />
+              </StackItem>
+            )}
           </Stack>
         )}
       </React.Fragment>

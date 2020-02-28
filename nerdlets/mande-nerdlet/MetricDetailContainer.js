46c77d4e4da1331bace196591db0ce890eba250c
Anna Strom-Olsen <aso1124@gmail.com>
2020-02-28 13:44:57 -0500

    feat: show metric detail pane
    Initial revision

---
+++ nerdlets/mande-nerdlet/MetricDetailContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const metricDetailContainer = props => {
+  return <div className="detail-panel">Detail view for {props.stack.title}</div>
+}
+
+export default metricDetailContainer

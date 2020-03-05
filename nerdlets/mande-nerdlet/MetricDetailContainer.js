08e79f02b20466152475601bbc41c93f6c6aa4d3
Anna Strom-Olsen <aso1124@gmail.com>
2020-03-05 13:34:23 -0500

    chore: code cleanup
    - remove unused code
    - tweak css naming

--- nerdlets/mande-nerdlet/MetricDetailContainer.js
+++ nerdlets/mande-nerdlet/MetricDetailContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Stack, StackItem } from 'nr1'
-import { Metric, BlankMetric } from '../../components/metric/Metric'
+import { Metric } from '../../components/metric/Metric'
 
 const metricDetailContainer = props => {
   const { stack, accountId, duration, threshold } = props
@@ -29,7 +29,6 @@ const metricDetailContainer = props => {
         return arr.concat(val)
       }, [])
 
-  console.info('MetricDetailContainer metrics', metrics)
   return (
     <Stack className="detail-container">
       <StackItem grow className="detail-filter">
@@ -40,47 +39,16 @@ const metricDetailContainer = props => {
         grow
         className="detail-content"
       >
-        <StackItem className="detail-metrics">
+        <StackItem className="detail-kpis">
           <Stack fullWidth={true}>{metrics}</Stack>
         </StackItem>
-        <StackItem className="detail-detail">
+        <StackItem className="detail-main">
           {stack.detailView && stack.detailView(props)}
           {!stack.detailView && <div />}
         </StackItem>
       </Stack>
     </Stack>
   )
-    // <Stack
-    //   directionType={Stack.DIRECTION_TYPE.VERTICAL}
-    //   gapType={Stack.GAP_TYPE.SMALL}
-    //   className="detail-panel"
-    // >
-      // <StackItem grow className="detail-title">
-      //   <span onClick={props.click} className="breadcrumb">
-      //     KPIs
-      //   </span>
-      //   <span>></span>
-      //   <span className="breadcrumb">{props.stack.title}</span>
-      // </StackItem>
-      // <Stack className="detail-container">
-      //   <StackItem grow className="detail-filter containing">
-      //     <p className="stackItem-title">FILTER PLACEHOLDER</p>
-      //   </StackItem>
-      //   <Stack
-      //     directionType={Stack.DIRECTION_TYPE.VERTICAL}
-      //     grow
-      //     className="detail-content"
-      //   >
-      //     <StackItem className="detail-metrics containing">
-      //       <Stack fullWidth={true}>{metrics}</Stack>
-      //     </StackItem>
-      //     <StackItem className="detail-detail containing">
-      //       <p className="stackItem-title">DETAILS PLACEHOLDER</p>
-      //     </StackItem>
-      //   </Stack>
-      // </Stack>
-    // </Stack>
-  
 }
 
 export default metricDetailContainer

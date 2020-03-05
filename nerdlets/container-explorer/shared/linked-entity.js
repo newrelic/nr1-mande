9557a7da6b2bf7d6398d95788777680b0e0b048d
Anna Strom-Olsen <aso1124@gmail.com>
2020-03-05 13:42:23 -0500

    chore: remove unused code
    Remove unused container-explorer

--- nerdlets/container-explorer/shared/linked-entity.js
+++
@@ -1,5 +0,0 @@
-import { Button, navigation } from 'nr1'
-
-export default function LinkedEntity({ title, entity, name, icon }) {
-  return <a className="entity-explorer-link" href="#" onClick={() => navigation.openStackedEntity(entity.guid)}>View in Entity Explorer</a>
-}

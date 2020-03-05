79662c8b7d7c964628a83a8b3c348125208b66d3
Anna Strom-Olsen <aso1124@gmail.com>
2020-03-05 13:36:01 -0500

    feat: add charts for video detail main view
    - also convert from Stack to Grid

--- components/metric-detail/VideoDetail.js
+++ components/metric-detail/VideoDetail.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
-import { AreaChart, PieChart, Stack, StackItem } from 'nr1'
+import { AreaChart, PieChart, Grid, GridItem } from 'nr1'
+import moment from 'moment'
+import momentDurationFormatSetup from 'moment-duration-format'
+momentDurationFormatSetup(moment)
 
 const metricDetail = props => {
   const { accountId, duration } = props
@@ -8,33 +11,69 @@ const metricDetail = props => {
   const since = ` SINCE ${duration} MINUTES AGO`
   const compare = ` COMPARE WITH ${duration} MINUTES AGO`
 
-  const timeToFirstFrame =
-    `SELECT filter(average(timeSinceRequested)/1000, WHERE actionName='CONTENT_START') as 'Seconds To First Frame', 
-    filter(count(*), WHERE actionName = 'CONTENT_REQUEST') as 'Total Requests' FROM PageAction TIMESERIES` +
+  const timingsNrql =
+    `SELECT filter(average(timeSinceLoad), WHERE actionName = 'CONTENT_REQUEST') as 'Time to Content Request', 
+     average(timeSinceLoad) as 'Time to Player Ready',
+     filter(average(timeSinceRequested)/1000, WHERE actionName='CONTENT_START') as 'Time To First Frame'
+     FROM PageAction TIMESERIES` + since
+  const countsNrql =
+    `SELECT filter(count(*), WHERE actionName = 'CONTENT_REQUEST') as 'Total Requests', 
+    filter(count(*), WHERE actionName = 'CONTENT_START') as 'Total Starts' 
+    FROM PageAction TIMESERIES` + since
+  const videoStartFailureNrql =
+    `SELECT count(*) AS 'Failures Before Start' FROM PageAction, MobileVideo, RokuVideo  WHERE actionName = 'CONTEN_ERROR' and contentPlayhead = 0 TIMESERIES` +
+    since
+  const rebufferNrql =
+    `SELECT filter(sum(timeSinceBufferBegin), WHERE actionName = 'CONTENT_BUFFER_END' and isInitialBuffering = 0) / filter(sum(playtimeSinceLastEvent), WHERE contentPlayhead is not null) as 'Rebuffer Ratio' FROM PageAction, MobileVideo, RokuVideo TIMESERIES` +
+    since
+  const interruptionNrql =
+    `SELECT filter(count(*), where actionName = 'CONTENT_BUFFER_START') / filter(count(*), where actionName = 'CONTENT_START') AS 'Interruption Ratio' FROM PageAction, MobileVideo, RokuVideo TIMESERIES` +
     since
 
-  const genericQuery =
-    'SELECT count(*) FROM PageAction FACET actionName' + since
-
-  console.info('timeToFirstFrame', timeToFirstFrame)
+  const addChart = (colSpans, title, chart) => {
+    return (
+      <GridItem columnSpan={colSpans}>
+        <div className="chart-container">
+          <div className="chart-title">
+            {title}
+            <div className="chart-subtitle">Since {duration} minutes ago</div>
+          </div>
+          <div className="detail-chart">{chart}</div>
+        </div>
+      </GridItem>
+    )
+  }
 
   return (
-    <Stack directionType={Stack.DIRECTION_TYPE.HORIZONTAL} fullWidth={true}>
-      <StackItem className="chart-container">
-        <AreaChart
-          className="detail-chart"
-          accountId={accountId}
-          query={timeToFirstFrame}
-        />
-      </StackItem>
-      <StackItem className="chart-container">
-        <PieChart
-          className="detail-chart pie-chart"
-          accountId={accountId}
-          query={genericQuery}
-        />
-      </StackItem>
-    </Stack>
+    <div className="detail-grid">
+      <Grid>
+        {addChart(
+          6,
+          'Content Request vs Player Ready vs Video Start (Average in Seconds)',
+          <AreaChart accountId={accountId} query={timingsNrql} />
+        )}
+        {addChart(
+          6,
+          'Total Requests vs Total Starts',
+          <AreaChart accountId={accountId} query={countsNrql} />
+        )}
+        {addChart(
+          4,
+          'Video Failures Before Start',
+          <AreaChart accountId={accountId} query={videoStartFailureNrql} />
+        )}
+        {addChart(
+          4,
+          'Rebuffer Ratio',
+          <AreaChart accountId={accountId} query={rebufferNrql} />
+        )}
+        {addChart(
+          4,
+          'Interruption Ratio',
+          <AreaChart accountId={accountId} query={interruptionNrql} />
+        )}
+      </Grid>
+    </div>
   )
 }
 

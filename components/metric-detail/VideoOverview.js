9145d387b9483f2cf221e24ceb48bb9489cd8c5e
Anna Strom-Olsen <aso1124@gmail.com>
2020-03-13 15:06:40 -0400

    fix: chart subtitle formatting and chart style
    - Correct the formatting applied to the timeframe shown in the chart subtitle to handle minutes, hours, days
    - add additional medium stlying to detail-chart div

--- components/metric-detail/VideoOverview.js
+++ components/metric-detail/VideoOverview.js
@@ -10,6 +10,11 @@ const videoOverview = props => {
 
   const since = ` SINCE ${duration} MINUTES AGO`
   const compare = ` COMPARE WITH ${duration} MINUTES AGO`
+  const formattedDuration = moment.duration(duration, 'minutes').format(() => {
+    if (duration > 1400) return 'd [days]'
+    if (duration > 60) return 'h [hours]'
+    else return 'm [minutes]'
+  })
 
   const timingsNrql =
     `SELECT filter(average(timeSinceLoad), WHERE actionName = 'CONTENT_REQUEST') as 'Time to Content Request', 
@@ -20,8 +25,9 @@ const videoOverview = props => {
     `SELECT filter(count(*), WHERE actionName = 'CONTENT_REQUEST') as 'Total Requests', 
     filter(count(*), WHERE actionName = 'CONTENT_START') as 'Total Starts' 
     FROM PageAction TIMESERIES` + since
-  const videoStartFailureNrql =
-    `SELECT count(*) AS 'Failures Before Start' FROM PageAction, MobileVideo, RokuVideo  WHERE actionName = 'CONTEN_ERROR' and contentPlayhead = 0 TIMESERIES` +
+  const videoErrorsNrql =
+    `SELECT filter(count(*), WHERE actionName = 'CONTENT_ERROR') / filter(count(*), WHERE actionName = 'CONTENT_REQUEST') as 'Video Errors', 
+    filter(count(*), WHERE actionName = 'CONTEN_ERROR' and contentPlayhead = 0) AS 'Failures Before Start' FROM PageAction, MobileVideo, RokuVideo TIMESERIES` +
     since
   const rebufferNrql =
     `SELECT filter(sum(timeSinceBufferBegin), WHERE actionName = 'CONTENT_BUFFER_END' and isInitialBuffering = 0) / filter(sum(playtimeSinceLastEvent), WHERE contentPlayhead is not null) as 'Rebuffer Ratio' FROM PageAction, MobileVideo, RokuVideo TIMESERIES` +
@@ -36,9 +42,9 @@ const videoOverview = props => {
         <div className="chart-container">
           <div className="chart-title">
             {title}
-            <div className="chart-subtitle">Since {duration} minutes ago</div>
+            <div className="chart-subtitle">Since {formattedDuration} ago</div>
           </div>
-          <div className="detail-chart">{chart}</div>
+          <div className="detail-chart medium">{chart}</div>
         </div>
       </GridItem>
     )
@@ -59,8 +65,8 @@ const videoOverview = props => {
         )}
         {addChart(
           4,
-          'Video Failures Before Start',
-          <AreaChart accountId={accountId} query={videoStartFailureNrql} />
+          'Video Errors and Failures Before Start',
+          <AreaChart accountId={accountId} query={videoErrorsNrql} />
         )}
         {addChart(
           4,

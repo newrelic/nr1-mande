c86fcfce5e337c8a684399a36c5002c75a9eeb2d
Anna Strom-Olsen <aso1124@gmail.com>
2020-03-16 16:09:15 -0400

    convert from function to react.component

--- nerdlets/mande-nerdlet/MetricDetailContainer.js
+++ nerdlets/mande-nerdlet/MetricDetailContainer.js
@@ -2,65 +2,67 @@ import React from 'react'
 
 import { Stack, StackItem } from 'nr1'
 import { Metric } from '../../components/metric/Metric'
+import { render } from 'react-dom'
 
-const metricDetailContainer = props => {
-  const {
-    stack,
-    accountId,
-    duration,
-    threshold,
-    activeMetric,
-    toggleMetric,
-  } = props
-
-  const metrics =
-    stack.metrics &&
-    stack.metrics
-      .map(metric => {
-        return [...Array(stack.metrics)].map((_, idx) => {
-          return (
-            <React.Fragment key={metric.title + idx}>
-              {metric.query && (
-                <Metric
-                  accountId={accountId}
-                  metric={metric}
-                  duration={duration}
-                  threshold={threshold}
-                  selected={activeMetric === metric.title}
-                  click={toggleMetric}
-                />
-              )}
-            </React.Fragment>
-          )
-        })
-      })
-      .reduce((arr, val) => {
-        return arr.concat(val)
-      }, [])
-
-  const detailView = () => {
-    if (activeMetric && stack.detailView) return stack.detailView(props)
-    if (stack.overview) return stack.overview(props)
+export default class MetricDetailContainer extends React.Component {
+  detailView = () => {
+    const { stack, activeMetric } = this.props
+    if (activeMetric && stack.detailView) return stack.detailView(this.props)
+    if (stack.overview) return stack.overview(this.props)
     return <div />
   }
 
-  return (
-    <Stack className="detail-container">
-      <StackItem grow className="detail-filter">
-        <p className="stackItem-title">FILTER PLACEHOLDER</p>
-      </StackItem>
-      <Stack
-        directionType={Stack.DIRECTION_TYPE.VERTICAL}
-        grow
-        className="detail-content"
-      >
-        <StackItem className="detail-kpis">
-          <Stack fullWidth={true}>{metrics}</Stack>
-        </StackItem>
-        <StackItem className="detail-main">{detailView()}</StackItem>
-      </Stack>
-    </Stack>
-  )
-}
+  render() {
+    const {
+      stack,
+      accountId,
+      duration,
+      threshold,
+      activeMetric,
+      toggleMetric,
+    } = this.props
 
-export default metricDetailContainer
+    const metrics =
+      stack.metrics &&
+      stack.metrics
+        .map(metric => {
+          return [...Array(stack.metrics)].map((_, idx) => {
+            return (
+              <React.Fragment key={metric.title + idx}>
+                {metric.query && (
+                  <Metric
+                    accountId={accountId}
+                    metric={metric}
+                    duration={duration}
+                    threshold={threshold}
+                    selected={activeMetric === metric.title}
+                    click={toggleMetric}
+                  />
+                )}
+              </React.Fragment>
+            )
+          })
+        })
+        .reduce((arr, val) => {
+          return arr.concat(val)
+        }, [])
+
+    return (
+      <Stack className="detail-container">
+        <StackItem grow className="detail-filter">
+          <p className="stackItem-title">FILTER PLACEHOLDER</p>
+        </StackItem>
+        <Stack
+          directionType={Stack.DIRECTION_TYPE.VERTICAL}
+          grow
+          className="detail-content"
+        >
+          <StackItem className="detail-kpis">
+            <Stack fullWidth={true}>{metrics}</Stack>
+          </StackItem>
+          <StackItem className="detail-main">{this.detailView()}</StackItem>
+        </Stack>
+      </Stack>
+    )
+  }
+}

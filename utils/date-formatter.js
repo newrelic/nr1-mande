e55d7ddb8ef984a08edb460e16bae26aefda522d
Anna Strom-Olsen <aso1124@gmail.com>
2020-03-24 12:35:52 -0400

    chore: remove debug statements

--- utils/date-formatter.js
+++ utils/date-formatter.js
@@ -3,7 +3,6 @@ import momentDurationFormatSetup from 'moment-duration-format'
 momentDurationFormatSetup(moment)
 
 export const dateFormatter = duration => {
-  console.info('in dataFormatter')
   return moment.duration(duration, 'minutes').format(() => {
     if (duration > 1400) return 'd [days]'
     if (duration > 60) return 'h [hours]'

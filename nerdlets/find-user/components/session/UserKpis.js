867d45e739929928a1613cab0efb08d517d8f509
Anna Strom-Olsen <aso1124@gmail.com>
2020-07-23 12:36:51 -0400

    update: find-user - user kpi group initial revision

---
+++ nerdlets/find-user/components/session/UserKpis.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Stack, Grid, GridItem } from 'nr1'
+import { roundToTwoDigits } from '../../../../utils/number-formatter'
+import MetricValue from '../../../../components/metric/MetricValue'
+
+export default class UserKpis extends React.Component {
+  render() {
+    const { sessionViews, userKpis } = this.props
+
+    const qosThreshold = {
+      critical: 80,
+      warning: 90,
+      type: 'below',
+    }
+
+    return (
+      <React.Fragment>
+        {userKpis && (
+          <Grid className="session-user-kpis">
+            <GridItem columnStart={1} columnEnd={4}>
+              <div className="sessionSectionBase  session-kpi-qos">
+                <div className="metric-chart">
+                  <div className="chart-title">
+                    Aggregate View Quality Score
+                  </div>
+                  <MetricValue
+                    threshold={qosThreshold}
+                    value={sessionViews.qualityScore}
+                    greenLight={true}
+                  />
+                </div>
+              </div>
+            </GridItem>
+            <GridItem columnStart={5} columnEnd={12}>
+              <Stack className="session-kpi-group" fullWidth>
+                {userKpis.map((kpi, idx) => {
+                  return (
+                    <div key={kpi.name + idx} className="sessionSectionBase">
+                      <div className="metric-chart">
+                        <div className="chart-title">{kpi.name}</div>
+                        <MetricValue
+                          threshold={kpi.threshold}
+                          value={
+                            // eslint-disable-next-line prettier/prettier
+                            roundToTwoDigits(kpi.value / kpi.viewCount)
+                          }
+                        />
+                      </div>
+                    </div>
+                  )
+                })}
+              </Stack>
+            </GridItem>
+          </Grid>
+        )}
+      </React.Fragment>
+    )
+  }
+}
+
+UserKpis.propTypes = {
+  sessionViews: PropTypes.array.isRequired,
+  userKpis: PropTypes.array.isRequired,
+}

bbc9c20d78404a54f00b97781f2f7c1115a4e28a
adwetzelnr <awetzel@newrelic.com>
2020-07-30 14:32:41 -0400

    added App Crashes - Android and App Crashes - iOS

--- config/DeviceConfig.js
+++ config/DeviceConfig.js
@@ -3,7 +3,7 @@ export default {
   title: 'Devices',
   metrics: [
     {
-      title: 'App Crashes',
+      title: 'App Crashes - Mobile',
       query: {
         nrql: `SELECT count(*) as 'result' FROM MobileCrash`,
         lookup: 'result',
@@ -13,6 +13,20 @@ export default {
       title: 'Javascript Error Rate',
       query: {
         nrql: `SELECT filter(count(*), WHERE eventType() = 'JavaScriptError') / filter(count(*), WHERE eventType() = 'PageView') * 100  as 'result' FROM JavaScriptError,PageView`,
+        lookup: 'result',
+      },
+    },
+    {
+      title: 'App Crashes - Android',
+      query: {
+        nrql: `SELECT count(*) as 'result' FROM MobileCrash where appName like \'%Android%\'`,
+        lookup: 'result',
+      },
+    },
+    {
+      title: 'App Crashes - iOS',
+      query: {
+        nrql: `SELECT count(*) as 'result' FROM MobileCrash where appName like \'%iOS%\'`,
         lookup: 'result',
       },
     },
